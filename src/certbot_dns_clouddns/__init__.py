8aeee01e47b7e536aa1e43233a2e1a91fba74f30
Radek SPRTA <sprta@vshosting.cz>
2020-02-14 13:52:04 +0100

    Update documentation

--- src/certbot_dns_clouddns/__init__.py
+++ src/certbot_dns_clouddns/__init__.py
@@ -1,20 +1,36 @@
 """
+certbot-dns-clouddns
+====================
+
 The `~certbot_dns_clouddns.dns_clouddns` plugin automates the process of
 completing a ``dns-01`` challenge (`~acme.challenges.DNS01`) by creating, and
-subsequently removing, TXT records using the CloudDNS REST API.
+subsequently removing, TXT records using the CloudDNS_ REST API.
+
+.. _CloudDNS: https://github.com/vshosting/clouddns
 
 
 Named Arguments
 ---------------
 
-========================================  =====================================
-``--dns-clouddns-credentials``            CloudDNS Remote API credentials_
-                                          INI file. (Required)
-``--dns-clouddns-propagation-seconds``    The number of seconds to wait for DNS
-                                          to propagate before asking the ACME
-                                          server to verify the DNS record.
-                                          (Default: 120)
-========================================  =====================================
+===========================================================  =====================================
+``--authenticator certbot-dns-clouddns:dns-clouddns``        Select the plugin. (Required)
+``--certbot-dns-clouddns:dns-clouddns-credentials``          CloudDNS Remote API credentials_
+                                                             INI file. (Required)
+``--certbot-dns-clouddns:dns-clouddns-propagation-seconds``  The number of seconds to wait for DNS
+                                                             to propagate before asking the ACME
+                                                             server to verify the DNS record.
+                                                             (Default: 60)
+===========================================================  =====================================
+
+
+Installation
+------------
+
+``certbot-dns-clouddns`` requires Python 3.5 or newer to run.
+
+.. code:: bash
+
+   pip install certbot-dns-clouddns
 
 
 Credentials
@@ -28,13 +44,14 @@ Example credentials file:
 .. code:: ini
 
    # CloudDNS API credentials used by Certbot
-   dns_clouddns_clientip = myclientid
-   dns_clouddns_email = myemailaddress
-   dns_clouddns_password = mysecretpassword
+   certbot-dns-clouddns:dns_clouddns_clientip = myclientid
+   certbot-dns-clouddns:dns_clouddns_email = myemailaddress
+   certbot-dns-clouddns:dns_clouddns_password = mysecretpassword
 
 The path to this file can be provided interactively or using the
-``--dns-clouddns-credentials`` command-line argument. Certbot records the path
-to this file for use during renewal, but does not store the file's contents.
+``--certbot-dns-clouddns:dns-clouddns-credentials`` command-line argument.
+Certbot records the path to this file for use during renewal, but does not store
+the file's contents.
 
 **Caution**
 
@@ -52,6 +69,7 @@ file. This warning will be emitted each time Certbot uses the credentials file,
 including for renewal, and cannot be silenced except by addressing the issue
 (e.g., by using a command like ``chmod 600`` to restrict access to the file).
 
+
 Examples
 --------
 
@@ -60,8 +78,8 @@ To acquire a certificate for ``example.com``
 .. code:: bash
 
    certbot certonly \
-     --dns-clouddns \
-     --dns-clouddns-credentials ~/.secrets/certbot/clouddns.ini \
+     --authenticator certbot-dns-clouddns:dns-clouddns \
+     --certbot-dns-clouddns:dns-clouddns-credentials ~/.secrets/certbot/clouddns.ini \
      -d example.com
 
 To acquire a single certificate for both ``example.com`` and ``*.example.com``
@@ -69,8 +87,8 @@ To acquire a single certificate for both ``example.com`` and ``*.example.com``
 .. code:: bash
 
    certbot certonly \
-     --dns-clouddns \
-     --dns-clouddns-credentials ~/.secrets/certbot/clouddns.ini \
+     --authenticator certbot-dns-clouddns:dns-clouddns \
+     --certbot-dns-clouddns:dns-clouddns-credentials ~/.secrets/certbot/clouddns.ini \
      -d example.com \
      -d '*.example.com'
 
@@ -79,8 +97,9 @@ To acquire a certificate for ``example.com``, waiting 240 seconds for DNS propag
 .. code:: bash
 
    certbot certonly \
-     --dns-clouddns \
-     --dns-clouddns-credentials ~/.secrets/certbot/clouddns.ini \
-     --dns-clouddns-propagation-seconds 240 \
+     --authenticator certbot-dns-clouddns:dns-clouddns \
+     --certbot-dns-clouddns:dns-clouddns-credentials ~/.secrets/certbot/clouddns.ini \
+     --certbot-dns-clouddns:dns-clouddns-propagation-seconds 240 \
      -d example.com
+
 """
